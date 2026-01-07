22611fcb6bc8ad95428bccc9a05bb3fbd3ea875e
maxmaxmax111 <info@maxgamestudio.com>
2026-01-07 14:05:32 -0500

    added a global scene to hold unit info for all units and admiral portraits

---
+++ global_unit_info.gd
@@ -0,0 +1,86 @@
+extends Node3D
+
+@export var demon_admiral_portrait: CompressedTexture2D
+@export var angel_admiral_portrait: CompressedTexture2D
+@export var alien_admiral_portrait: CompressedTexture2D
+@export var human_admiral_portrait: CompressedTexture2D
+
+@export var demon_pawn_portrait: CompressedTexture2D
+@export var demon_pawn_supply: int
+@export var demon_pawn_size: int
+
+@export var demon_knight_portrait: CompressedTexture2D
+@export var demon_knight_supply: int
+@export var demon_knight_size: int
+
+@export var demon_bishop_portrait: CompressedTexture2D
+@export var demon_bishop_supply: int
+@export var demon_bishop_size: int
+
+@export var demon_rook_portrait: CompressedTexture2D
+@export var demon_rook_supply: int
+@export var demon_rook_size: int
+
+@export var demon_queen_portrait: CompressedTexture2D
+@export var demon_queen_supply: int
+@export var demon_queen_size: int
+
+@export var angel_pawn_portrait: CompressedTexture2D
+@export var angel_pawn_supply: int
+@export var angel_pawn_size: int
+
+@export var angel_knight_portrait: CompressedTexture2D
+@export var angel_knight_supply: int
+@export var angel_knight_size: int
+
+@export var angel_bishop_portrait: CompressedTexture2D
+@export var angel_bishop_supply: int
+@export var angel_bishop_size: int
+
+@export var angel_rook_portrait: CompressedTexture2D
+@export var angel_rook_supply: int
+@export var angel_rook_size: int
+
+@export var angel_queen_portrait: CompressedTexture2D
+@export var angel_queen_supply: int
+@export var angel_queen_size: int
+
+@export var alien_pawn_portrait: CompressedTexture2D
+@export var alien_pawn_supply: int
+@export var alien_pawn_size: int
+
+@export var alien_knight_portrait: CompressedTexture2D
+@export var alien_knight_supply: int
+@export var alien_knight_size: int
+
+@export var alien_bishop_portrait: CompressedTexture2D
+@export var alien_bishop_supply: int
+@export var alien_bishop_size: int
+
+@export var alien_rook_portrait: CompressedTexture2D
+@export var alien_rook_supply: int
+@export var alien_rook_size: int
+
+@export var alien_queen_portrait: CompressedTexture2D
+@export var alien_queen_supply: int
+@export var alien_queen_size: int
+
+@export var human_pawn_portrait: CompressedTexture2D
+@export var human_pawn_supply: int
+@export var human_pawn_size: int
+
+@export var human_knight_portrait: CompressedTexture2D
+@export var humandemon_knight_supply: int
+@export var human_knight_size: int
+
+@export var human_bishop_portrait: CompressedTexture2D
+@export var human_bishop_supply: int
+@export var human_bishop_size: int
+
+@export var human_rook_portrait: CompressedTexture2D
+@export var human_rook_supply: int
+@export var human_rook_size: int
+
+@export var human_queen_portrait: CompressedTexture2D
+@export var humandemon_queen_supply: int
+@export var human_queen_size: int
